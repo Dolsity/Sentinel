fb95c8e215186136766259db3e8080c3775e4feb
Jay <Acorder43812@gmail.com>
2023-05-28 00:02:44 -0700

    Update info.py

--- cogs/commands/info/info.py
+++ cogs/commands/info/info.py
@@ -1,6 +1,7 @@
 from nextcord.ext import commands
 import nextcord
 from nextcord import Interaction
+from nextcord.utils import format_dt
 from utils.config import embed_color, owner_id
 
 class Utility(commands.Cog, name="Utility", description="Utility Commands"):
@@ -11,7 +12,7 @@ class Utility(commands.Cog, name="Utility", description="Utility Commands"):
 
     @commands.command(name="ping")
     async def command_ping(self, interaction : Interaction):
-        """Latency of Fora"""
+        """Latency of the bot"""
         await interaction.trigger_typing()
         embed = nextcord.Embed(
             title=f":ping_pong: Pong!", color=embed_color
@@ -21,7 +22,8 @@ class Utility(commands.Cog, name="Utility", description="Utility Commands"):
             value=f"{round(self.bot.latency*1000)}ms", inline=False
         )
         embed.set_thumbnail(url=self.bot.user.display_avatar)
-        await interaction.send(embed=embed)
+        await interaction.reply(embed=embed, mention_author=False)
+
 
     @commands.command(name="about", aliases=["information"])
     async def command_about(self, interaction : Interaction):
@@ -45,7 +47,7 @@ class Utility(commands.Cog, name="Utility", description="Utility Commands"):
 
         embed.add_field(
             name="Information", 
-            value=f"**» Developer:** {owners}\n", inline=False
+            value=f"**» Developer(s):** {owners}\n", inline=False
         )
         embed.add_field(
             name="Statistics",
@@ -53,7 +55,126 @@ class Utility(commands.Cog, name="Utility", description="Utility Commands"):
             value=f"**» Total Users:** {len(list(self.bot.get_all_members()))}\n"
                   f"**» API Latency:** {round(self.bot.latency*1000)}ms", inline=False
         )
-        await interaction.send(embed=embed)
+        await interaction.reply(embed=embed, mention_author=False)
+
+
+    @commands.command(name="userinfo", aliases=["ui"])
+    async def command_userinfo(self, interaction : Interaction, user : nextcord.Member = None):
+        """Information about a user"""
+        await interaction.trigger_typing()
+
+        user = user or interaction.author
+
+        embed = nextcord.Embed(
+            title=f"Information about {user.name}", color=embed_color
+        )
+        embed.set_author(
+            name=user.name, icon_url=user.display_avatar
+        )
         
+        embed.add_field(
+            name="Basic Information",
+            value=f"**User:** {user.mention} ({user.id})\n"
+                  f"**Created:** {format_dt(user.created_at, style='R')}\n"
+                  f"**Joined:** {format_dt(user.joined_at, style='R')}\n"
+                  f"**Top Role:** {user.top_role.mention}", inline=False
+        )
+        roles = [role.mention for role in user.roles[1:]]
+        roles.reverse()
+        roles = ", ".join(roles)
+        embed.add_field(
+            name="Roles",
+            value=f"{roles}", inline=False
+        )
+        embed.set_thumbnail(url=user.display_avatar)
+        await interaction.reply(embed=embed, mention_author=False)
+
+
+    @commands.command(name="serverinfo", aliases=["si"])
+    async def command_serverinfo(self, interaction : Interaction):
+        """Information about the server"""
+        await interaction.trigger_typing()
+
+        embed = nextcord.Embed(
+            description=f"{interaction.guild.name} ({interaction.guild.id})", color=embed_color
+        )
+        embed.set_author(
+            name=interaction.guild.name, icon_url=interaction.guild.icon
+        )
+
+        embed.add_field(
+            name="Basic Information",
+            value=f"**Created:** {format_dt(interaction.guild.created_at, style='R')}\n"
+                  f"**Owner:** {interaction.guild.owner.mention}\n"
+                  f"**Members:** {interaction.guild.member_count}\n"
+                  f"**Channels:** {len(interaction.guild.channels)}\n"
+                  f"**Roles:** {len(interaction.guild.roles)}\n"
+                  f"**Emojis:** {len(interaction.guild.emojis)}\n"
+                  f"**Boosts:** {interaction.guild.premium_subscription_count}\n"
+                  f"**Boost Level:** {interaction.guild.premium_tier}", inline=False
+        )
+        embed.set_thumbnail(url=interaction.guild.icon)
+
+        await interaction.reply(embed=embed, mention_author=False)
+
+
+    @commands.command(name="avatar", aliases=["av"])
+    async def command_avatar(self, interaction : Interaction, user : nextcord.Member = None):
+        """Get a user's avatar"""
+        await interaction.trigger_typing()
+
+        user = user or interaction.author
+
+        embed = nextcord.Embed(
+            title=f"{user.name}'s avatar", color=embed_color
+        )
+        embed.set_image(url=user.display_avatar)
+
+        await interaction.reply(embed=embed, mention_author=False)
+
+    
+    @commands.command(name="roleinfo", aliases=["ri"])
+    async def command_roleinfo(self, interaction : Interaction, role : nextcord.Role):
+        """Information about a role"""
+        await interaction.trigger_typing()
+
+        embed = nextcord.Embed(
+            title=f"Information about {role.name}", color=embed_color
+        )
+        embed.add_field(
+            name="Basic Information",
+            value=f"**Name:** {role.mention}\n"
+                  f"**ID:** {role.id}\n"
+                  f"**Created:** {format_dt(role.created_at, style='R')}\n"
+                  f"**Color:** {role.color}\n"
+                  f"**Position:** {role.position}\n"
+                  f"**Mentionable:** {role.mentionable}\n", inline=False
+        )
+        embed.set_thumbnail(url=interaction.guild.icon)
+        await interaction.reply(embed=embed, mention_author=False)
+
+
+    @commands.command(name="channelinfo", aliases=["ci"])
+    async def command_channelinfo(self, interaction : Interaction, channel : nextcord.TextChannel = None):
+        """Information about a channel"""
+        await interaction.trigger_typing()
+
+        channel = channel or interaction.channel
+
+        embed = nextcord.Embed(
+            title=f"Information about {channel.name}", color=embed_color
+        )
+        embed.add_field(
+            name="Basic Information",
+            value=f"**Name:** {channel.mention}\n"
+                  f"**ID:** {channel.id}\n"
+                  f"**Created:** {format_dt(channel.created_at, style='R')}\n"
+                  f"**Category:** {channel.category}\n"
+                  f"**Position:** {channel.position}\n"
+                  f"**NSFW:** {channel.is_nsfw()}\n", inline=False
+        )
+        embed.set_thumbnail(url=interaction.guild.icon)
+        await interaction.reply(embed=embed, mention_author=False)
+
 def setup(bot):
     bot.add_cog(Utility(bot))
